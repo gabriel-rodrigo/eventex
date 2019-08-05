e265337089adc569374d4075adbfe26307bffee4
Gabriel Rodrigo <gabrielrodrigodcs@gmail.com>
2019-08-05 15:52:55 -0300

    Adiciona campo paid

--- eventex/subscriptions/admin.py
+++ eventex/subscriptions/admin.py
@@ -5,7 +5,7 @@ from eventex.subscriptions.models import Subscription
 
 
 class SubscriptionModelAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email', 'phone', 'cpf', 'created_at', 'subscribed_today')
+    list_display = ('name', 'email', 'phone', 'cpf', 'created_at', 'subscribed_today', 'paid')
     date_hierarchy = 'created_at'
     search_fields = ('name', 'email', 'phone', 'cpf', 'created_at')
 
@@ -14,7 +14,7 @@ class SubscriptionModelAdmin(admin.ModelAdmin):
 
     subscribed_today.short_description = 'inscrito hoje?'
     subscribed_today.boolean = True
-    list_filter = ('created_at',)
+    list_filter = ('paid', 'created_at')
 
 
 admin.site.register(Subscription, SubscriptionModelAdmin)
